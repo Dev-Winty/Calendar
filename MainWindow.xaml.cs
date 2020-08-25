1e1a083d049c8c9e3fb7bcedc51746a8e2604906
LimKyeongJoon <kyeongjoon16881688@gmail.com>
2020-08-25 09:59:35 +0900

    코드 리팩토링 후 파일 입출력
    코드 리팩토링 후 Save.txt에 날짜 정보 저장

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -20,75 +21,22 @@ namespace Calendar
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {   
+        public string dayOfWeek = "";
+        public int date = 0;
+        public List<Day> days = new List<Day>();
         public MainWindow()
         {   
             InitializeComponent();
             this.DataContext = DataContext;
-            int date = 1;
-            int i, j;
-
-            for (i = 0; i < 5; i++)
-            {
-                for (j = 0; j < 7; j++)
-                {
-                    
-                    StackPanel stackPanel = new StackPanel();
-                    Border border = new Border()
-                    {
-                        CornerRadius = new CornerRadius(20),
-                        BorderThickness = new Thickness()
-                        {
-                            Bottom = 2,
-                            Left = 2,
-                            Right = 2,
-                            Top = 2
-                        },
-                        BorderBrush = new SolidColorBrush(Colors.Silver) { Opacity = 0.7 },
-                        Margin = new Thickness(2),
-                        Padding = new Thickness(4)
-                    };
-                    TextBlock textBlock = new TextBlock();
-                    textBlock.HorizontalAlignment = HorizontalAlignment.Center;
-                    switch (date)
-                    {
-                        case 1:
-                            textBlock.Text = "일요일";
-                            break;
-                        case 2:
-                            textBlock.Text = "월요일";
-                            break;
-                        case 3:
-                            textBlock.Text = "화요일";
-                            break;
-                        case 4:
-                            textBlock.Text = "수요일";
-                            break;
-                        case 5:
-                            textBlock.Text = "목요일";
-                            break;
-                        case 6:
-                            textBlock.Text = "금요일";
-                            break;
-                        case 7:
-                            textBlock.Text = "토요일";
-                            date = 0;
-                            break;
-                    }
-                    date++;
-                    stackPanel.Children.Add(textBlock);
-                    border.Child = stackPanel;
-                    border.SetValue(Grid.RowProperty, i);
-                    border.SetValue(Grid.ColumnProperty, j);
-                    gridMain.Children.Add(border);
-                }
-            }
-
+            loadCalendar();
+            printCalendar();
             
         }
 
         private void exitBtn_Click(object sender, RoutedEventArgs e)
         {
+            saveCalendar();
             System.Windows.Application.Current.Shutdown();
         }
 
@@ -96,5 +44,90 @@ namespace Calendar
         {
             WindowState = WindowState.Minimized;
         }
+        public void printCalendar()
+        {
+            int i;
+
+            for (i = 0; i < days.Count; i++)
+            {
+                StackPanel stackPanel = new StackPanel();
+                Border border = new Border()
+                {
+                    CornerRadius = new CornerRadius(20),
+                    BorderThickness = new Thickness()
+                    {
+                        Bottom = 2,
+                        Left = 2,
+                        Right = 2,
+                        Top = 2
+                    },
+                    BorderBrush = new SolidColorBrush(Colors.Silver) { Opacity = 0.7 },
+                    Margin = new Thickness(2),
+                    Padding = new Thickness(4)
+                };
+                TextBlock title = new TextBlock();
+                TextBlock contents = new TextBlock();
+                title.HorizontalAlignment = HorizontalAlignment.Center;
+                title.FontWeight = FontWeights.Bold;
+                contents.HorizontalAlignment = HorizontalAlignment.Center;
+                
+                border.Name = "index" + days[i].date.ToString();
+                title.Text = days[i].date.ToString() + " " + days[i].dayOfWeek;
+                contents.Text = days[i].contents;
+                stackPanel.Children.Add(title);
+                stackPanel.Children.Add(contents);
+                border.Child = stackPanel;
+                border.SetValue(Grid.RowProperty, days[i].row);
+                border.SetValue(Grid.ColumnProperty, days[i].column);
+                gridMain.Children.Add(border);
+            }
+        }
+
+        public void saveCalendar()
+        {
+            StreamWriter writer = new StreamWriter("Save.txt");
+            int i;
+
+            for (i = 0; i < days.Count; i++)
+            {
+                writer.WriteLine(days[i].date);
+                writer.WriteLine(days[i].dayOfWeek);
+                writer.WriteLine(days[i].contents);
+                writer.WriteLine(days[i].row);
+                writer.WriteLine(days[i].column);
+            }
+            writer.Close();
+
+        }
+
+        public void loadCalendar()
+        {
+            StreamReader reader = new StreamReader("Save.txt");
+            int date = 0;
+            string dayOfWeek = "";
+            string contents = "";
+            int row = 0, column = 0;
+            while (reader.Peek() >= 0)
+            {   
+              
+                date = Int32.Parse(reader.ReadLine());
+                MessageBox.Show(date.ToString());
+                dayOfWeek = reader.ReadLine();
+                contents = reader.ReadLine();
+                row = Convert.ToInt32(reader.ReadLine());
+                column = Convert.ToInt32(reader.ReadLine());
+                days.Add(new Day { date = date, dayOfWeek = dayOfWeek, contents = contents, row = row, column = column });
+            }         
+            //MessageBox.Show(days[0].date.ToString() + " " + days[0].row.ToString() + " " + days[0].column.ToString());
+            reader.Close();
+        }
+    }
+    public class Day
+    {
+        public string dayOfWeek { get; set; }
+        public int date { get; set; }
+        public string contents { get; set; }
+        public int row { get; set; }
+        public int column { get; set; }
     }
 }
