f14bd0310346a8698aaaac1c32cb74617cfa5785
LimKyeongJoon <kyeongjoon16881688@gmail.com>
2020-08-25 11:13:29 +0900

    "일정 추가" 기능 다듬기
    "일정 추가"를 다듬었다

--- AddDay.xaml.cs
+++ AddDay.xaml.cs
@@ -23,7 +23,6 @@ namespace Calendar
         public AddDay()
         {
             InitializeComponent();
-            MessageBox.Show(((MainWindow)System.Windows.Application.Current.MainWindow).days[1].contents);
         }
 
         private void addBtn_Click(object sender, RoutedEventArgs e)
@@ -36,7 +35,8 @@ namespace Calendar
 
             mainWindow.days.Add(new Day { date = date, dayOfWeek = dayOfWeek, contents = cotents, row = row, column = column });
             mainWindow.saveCalendar();
-            mainWindow.printCalendar();            
+            mainWindow.printCalendar();
+            this.Close();
         }
     }
 }
